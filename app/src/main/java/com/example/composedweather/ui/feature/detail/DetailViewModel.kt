b11b56a16ed82b976ffb6566709c246de0c0fd27
MSI KATANA <gaudentas199@gmail.com>
2023-10-19 05:43:29 +0530

    Setting up response for multiple fields of data as per the request

--- app/src/main/java/com/example/composedweather/ui/feature/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/composedweather/ui/feature/detail/DetailViewModel.kt
@@ -2,8 +2,6 @@ package com.example.composedweather.ui.feature.detail
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
